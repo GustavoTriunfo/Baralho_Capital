1253844c5426fd0503df38f003926c0fc04f6d81
Gustavo Costa <gu.triunfo@gmail.com>
2024-03-06 22:52:36 -0300

    menu lateral adicionado

--- script.js
+++ script.js
@@ -20,3 +20,30 @@
       }
     });
   });
+
+  function toggleMenu() {
+    console.log("Toggle menu clicked");
+    var overlay = document.getElementById("menuOverlay");
+    if (overlay.classList.contains("show")) {
+        overlay.classList.remove("show");
+        overlay.style.display = "none"; // Se a classe "show" estiver presente, oculta o menu
+    } else {
+        overlay.classList.add("show");
+        overlay.style.display = "block"; // Se a classe "show" não estiver presente, mostra o menu
+    }
+}
+
+function option1() {
+    // Ação para a opção 1
+    alert("Opção 1 selecionada");
+}
+
+function option2() {
+    // Ação para a opção 2
+    alert("Opção 2 selecionada");
+}
+
+function option3() {
+    // Ação para a opção 3
+    alert("Opção 3 selecionada");
+}
